145f2c97b2aaaa90799396efa2d049455024a70a
Joshua J. <jjacksonSWE@outlook.com>
2026-08-07 14:51:20 -0400

    Create solution.py

---
+++ solutions/0394-decode-string/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char != "]":
+                stack.append(char)
+            
+            else:
+                curr_str = ""
+                while stack[-1] != "[":
+                    curr_str = stack.pop() + curr_str
+                
+                stack.pop()
+                
+                curr_num = ""
+                while stack and stack[-1].isdigit():
+                    curr_num = stack.pop() + curr_num
+                stack.append(curr_str * int(curr_num))
+
+        return "".join(stack)
