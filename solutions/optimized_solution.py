780af8da4ae415ad86d3eda934470e6a43e50d17
Joshua J. <jjacksonSWE@outlook.com>
2026-08-17 17:00:44 -0400

    Create optimized_solution.py

---
+++ solutions/optimized_solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left: int = 0
+        right: int = len(nums) - 1
+
+        while left < right:
+            mid: int = left + (right - left) // 2
+
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return nums[left]
